d7b1ef4efda48318ecd95e4c7ab291f5667b5493
Raaghav Agarwal <Raaghav.Agarwal@gmail.com>
2020-05-03 21:47:32 -0600

    Trying to use the database

--- src/org/foodbutler/db/DBHelper.java
+++ src/org/foodbutler/db/DBHelper.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import org.tinylog.Logger;
 
 public class DBHelper {
-	private final String url = "jdbc:postgresql://localhost:5432/FoodButler";
+	private final String url = "postgres://liyyjhbihezjus:e511477a502a343f368e5309b7c85722ee5c33e9068d65ff0c7cbfee872db85b@ec2-18-235-20-228.compute-1.amazonaws.com:5432/deiakrethhr73";
 	private final String user = "butler";
 	private final String password = "foodbutler";
 	
