6353acdbc74ccc19dd4e484e4d560839feb0b297
Raaghav Agarwal <Raaghav.Agarwal@gmail.com>
2020-05-23 20:07:26 -0600

    Trying to get aws database url

--- src/org/foodbutler/FoodButler.java
+++ src/org/foodbutler/FoodButler.java
@@ -5,12 +5,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
+import org.foodbutler.db.DBHelper;
+
+
+
+
 
 @Path("butler")
 public class FoodButler {
 	@GET
 	@Path("search")
-	public int getResults(@QueryParam("search")String search){
-		return 0;
+	public String getResults(@QueryParam("search")String search){
+		DBHelper result = new DBHelper();
+		return result.getClosestStore(search,40.15, -105.14);
 	}
 }
--- src/org/foodbutler/db/DBHelper.java
+++ src/org/foodbutler/db/DBHelper.java
@@ -23,6 +23,7 @@ public class DBHelper {
         Connection conn = null;
         try {
             URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            Logger.info(dbUri);
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
             String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
