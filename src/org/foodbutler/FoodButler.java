b4bb5324d1acf1af870aa733a2e83cd2834e8844
Raaghav Agarwal <Raaghav.Agarwal@gmail.com>
2020-05-23 19:46:54 -0600

    Taking out testing code

--- src/org/foodbutler/FoodButler.java
+++ src/org/foodbutler/FoodButler.java
@@ -5,14 +5,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-import org.foodbutler.db.DBHelper;
 
 @Path("butler")
 public class FoodButler {
 	@GET
 	@Path("search")
 	public int getResults(@QueryParam("search")String search){
-		DBHelper result = new DBHelper();
 		return 0;
 	}
 }
